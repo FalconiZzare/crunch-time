75acfeec26b2ff4a254f1ed264c6d63b5875f551
Talat Mahmud <talatcbf@gmail.com>
2025-04-15 00:49:37 +0600

    add: login suspense3

--- app/sign-in/GoogleProvider.jsx
+++ app/sign-in/GoogleProvider.jsx
@@ -1,12 +1,12 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { Icons } from "@/components/Icons";
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
-import { useSearchParams } from "next/navigation";
 
-const GoogleProvider = () => {
+const GoogleSignInButton = () => {
+  const { useSearchParams } = require("next/navigation");
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl") || "/";
 
@@ -28,6 +28,29 @@ const GoogleProvider = () => {
       <Icons.google />
       Continue with Google
     </Button>
+  );
+};
+
+const ButtonFallback = () => {
+  return (
+    <Button
+      variant={"outline"}
+      className={
+        "mt-4 w-full items-center justify-center gap-5 py-5 text-base md:w-10/12 [&_svg]:size-5"
+      }
+      disabled
+    >
+      <Icons.google />
+      Loading...
+    </Button>
+  );
+};
+
+const GoogleProvider = () => {
+  return (
+    <Suspense fallback={<ButtonFallback />}>
+      <GoogleSignInButton />
+    </Suspense>
   );
 };
 
